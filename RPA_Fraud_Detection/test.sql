42fdb5e9bbed5239168a76cc21aa8481988a0423
kyle67 <daniels.kyle@gmail.com>
2020-09-12 16:10:36 -0400

    wrote query to find full_names with Art_Vandelay or der as middle name

--- RPA_Fraud_Detection/test.sql
+++ RPA_Fraud_Detection/test.sql
@@ -5,3 +5,7 @@ SELECT *
  SELECT full_name, email
  FROM transaction_data
  WHERE zip = 20252;
+
+ SELECT full_name, email
+ FROM transaction_data
+ WHERE full_name = 'Art Vandelay' OR full_name LIKE '% der %'; 
